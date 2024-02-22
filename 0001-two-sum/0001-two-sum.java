a0debac329b70acd442233eeac5fbae81e1c3110
YERI <jyr3177@gmail.com>
2024-02-23 00:11:19 +0900

    Time: 44 ms (41.82%), Space: 45 MB (34.86%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,12 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i=0; i<nums.length-1; i++){
-            for(int j=i+1; j<nums.length; j++){
-                if(nums[i]+nums[j] == target){
-                    return new int[]{i, j};
+
+        for(int i=0; i<nums.length-1; i++){ // 0부터 nums길이-1까지 반복
+            for(int j=i+1; j<nums.length; j++){ // 1부터 num길이까지 반복
+                if(nums[i]+nums[j]==target){ // target 값이 맞으면 
+                    return new int[]{i,j}; // 인덱스 번호 리턴 
                 }
             }
         }
-        return new int[]{};
+
+        return new int[]{0, 0}; // target 못찾을시
     }
 }
