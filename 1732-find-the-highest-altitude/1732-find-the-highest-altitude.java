fa2fdd1afb98e4c104be4f16013f71340ee200fb
YERI <jyr3177@gmail.com>
2024-02-24 10:00:58 +0900

    Time: 0 ms (100.00%), Space: 41 MB (75.12%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -1,12 +1,28 @@
 class Solution {
+    /*
+    // [1]
     public int largestAltitude(int[] gain) {
-        int[] altitudes = new int[gain.length+1];
+        int[] altitudes = new int[gain.length+1]; // 고도 배열 
         altitudes[0]=0;
         int max=0;
         
         for(int i=0; i<gain.length; i++){
             altitudes[i+1]=altitudes[i]+gain[i];
             max=Math.max(max, altitudes[i+1]);
+        }
+        
+        //System.out.println(Arrays.toString(altitudes));
+        return max;
+    }
+    */
+    // [2]
+    public int largestAltitude(int[] gain) {
+        int max=0;
+        int temp=0;
+        
+        for(int i=0; i<gain.length; i++){
+            temp+=gain[i];
+            max=Math.max(max, temp);
         }
         
         //System.out.println(Arrays.toString(altitudes));
