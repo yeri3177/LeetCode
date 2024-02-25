5d87d3034e65645ae35aaefbe8ba65ba105b0723
YERI <jyr3177@gmail.com>
2024-02-26 03:07:54 +0900

    Time: 1 ms (99.44%), Space: 43.9 MB (55.46%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int compress(char[] chars) {
+        StringBuilder str = new StringBuilder();
+        for(int i = 0; i < chars.length; ) {
+            char c = chars[i];
+            int cnt = 1;
+            while (i + 1 < chars.length && c == chars[i + 1]) {
+                i++;
+                cnt++;
+            }
+            str.append(c);
+            if (cnt > 1) {
+                str.append(cnt);
+            }
+            i++;
+        }
+        char[] comp = str.toString().toCharArray();
+        for(int i=0;i<comp.length;i++){
+            chars[i]=comp[i];
+        }
+        
+        return comp.length;
+    }
+}
