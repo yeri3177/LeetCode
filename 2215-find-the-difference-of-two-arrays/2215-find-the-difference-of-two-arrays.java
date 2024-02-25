58c9752ed7381892a16d00feaecca245fd6af397
YERI <jyr3177@gmail.com>
2024-02-25 16:15:27 +0900

    Time: 15 ms (33.24%), Space: 45.9 MB (8.23%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
@@ -1,4 +1,6 @@
 class Solution {
+    /*
+    // 361ms(Beats 5.01%) / 45.43MB(Beats 43.25%)
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
         // 리턴값 넣을 객체
         List<List<Integer>> answer = new ArrayList<>();
@@ -9,14 +11,14 @@ class Solution {
         List<Integer> l1 = IntStream.of(nums1).boxed().collect(Collectors.toList());
         List<Integer> l2 = IntStream.of(nums2).boxed().collect(Collectors.toList());
         
-        // list1 구하기
+        // 리턴값 list1 구하기
         for(int i=0; i<l1.size(); i++){
             if(!l2.contains(l1.get(i)) && !list1.contains(l1.get(i))){
                 list1.add(l1.get(i));
             }
         }
 
-        // list2 구하기
+        // 리턴값 list2 구하기
         for(int i=0; i<l2.size(); i++){
             if(!l1.contains(l2.get(i)) && !list2.contains(l2.get(i))){
                 list2.add(l2.get(i));
@@ -28,5 +30,25 @@ class Solution {
         answer.add(list2);
 
         return answer;
+    }
+    */
+
+    // 16ms(Beats 33.24%) / 45.36MB(Beats 61.31%)
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        // 매개변수를 Set에 넣어 중복 제거 
+        Set<Integer> set1 = Arrays.stream(nums1).boxed().collect(Collectors.toSet());
+        Set<Integer> set2 = Arrays.stream(nums2).boxed().collect(Collectors.toSet());
+
+        Set<Integer> dupSets = new HashSet<>(); // 중복된 값 찾기 
+        for(int num : set1) {
+            if(set2.contains(num)) {
+                dupSets.add(num);
+            }
+        }
+        // 중복 제외하여 유일한 값만 남기기
+        set1.removeAll(dupSets); 
+        set2.removeAll(dupSets);
+
+        return Arrays.asList(new ArrayList<>(set1), new ArrayList<>(set2));
     }
 }
