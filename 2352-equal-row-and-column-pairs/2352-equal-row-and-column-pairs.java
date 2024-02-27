aedcd3138300234f23afae05c51faf531e5806ea
YERI <jyr3177@gmail.com>
2024-02-27 09:38:29 +0900

    Time: 7 ms (97.34%), Space: 50.6 MB (51.83%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -1,7 +1,7 @@
 class Solution {
     public int equalPairs(int[][] grid) {
         int ans = 0;
-        int n = grid[0].length;
+        int n = grid.length;
 
         for(int i=0; i<n; i++){
             int[] arr = new int[n]; // 열 배열
@@ -9,12 +9,11 @@ class Solution {
                 arr[j] = grid[j][i];
             }
 
+            // grid[k] : 행 <-> arr : 열 
             for(int k=0; k<n; k++){ // 열 고정, 행 배열 하나씩 맞는지 확인 
-                // grid[k] : 행 <-> arr : 열 
                 if(Arrays.equals(grid[k], arr)) ans++;
             }
         }
-        
         return ans;
     }
 }
