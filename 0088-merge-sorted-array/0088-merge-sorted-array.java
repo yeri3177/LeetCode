fbc99b37e917096b77e07f1c2e1d9a0fa15bf585
YERI <jyr3177@gmail.com>
2024-03-01 10:20:54 +0900

    Time: 1 ms (35.55%), Space: 41.9 MB (78.39%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,4 +1,6 @@
 class Solution {
+    /*
+    // 내가 한 방법 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         
         // nums1의 0 요소 자리에 nums2의 요소 넣기 
@@ -11,6 +13,17 @@ class Solution {
         }
 
         // 배열 정렬
+        Arrays.sort(nums1);
+    }
+    */
+
+    // upvote most
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        for (int j = 0, i = m; j < n; j++) {   // i = m !!! 
+            nums1[i] = nums2[j];
+            i++;
+        }
         Arrays.sort(nums1);
     }
 }
